a55731e97e3c892a5c58e0403ad163e71c2d260b
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-09-22 06:53:27 +0530

    terraform vpc module development

--- variables.tf
+++ variables.tf
@@ -24,3 +24,19 @@ variable "public_subnet_tags"{
     type= map(string)
     default = {}
 }
+variable "private_subnet_cidrs"{
+    type= list(string)
+    default=["10.0.11.0/24", "10.0.12.0/24"]
+}
+variable "private_subnet_tags"{
+    type= map(string)
+    default = {}
+}
+variable "database_subnet_cidrs"{
+    type= list(string)
+    default=["10.0.21.0/24", "10.0.22.0/24"]
+}
+variable "database_subnet_tags"{
+    type= map(string)
+    default = {}
+}
--- vpc.tf
+++ vpc.tf
@@ -41,3 +41,37 @@ resource "aws_subnet" "public" {
   )
 }
 
+#private_subnets
+resource "aws_subnet" "private" {
+  count= length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone= local.azs_name[count.index]
+  # map_public_ip_on_launch= true
+
+  tags = merge(
+    var.private_subnet_tags,
+    local.common_tags,
+    {
+      Name= "${local.common_name}-private-${split("-", local.azs_name[count.index])[2]}"  #"us-east-1a
+    }
+  )
+}
+
+#database_subnets
+resource "aws_subnet" "database" {
+  count= length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone= local.azs_name[count.index]
+  # map_public_ip_on_launch= true
+
+  tags = merge(
+    var.database_subnet_tags,
+    local.common_tags,
+    {
+      Name= "${local.common_name}-database-${split("-", local.azs_name[count.index])[2]}"  #"us-east-1a
+    }
+  )
+}
+
